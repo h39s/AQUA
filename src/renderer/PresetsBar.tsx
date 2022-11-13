28f974d2773bc8edccff9954f3ef1235da4ed182
Selina Li <selinali2010@gmail.com>
2022-11-12 18:57:24 -0800

    Add icons to preset bar list items and refactor List

--- src/renderer/PresetsBar.tsx
+++ src/renderer/PresetsBar.tsx
@@ -1,27 +1,83 @@
-import { useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import './styles/PresetsBar.scss';
 import { useAquaContext } from './utils/AquaContext';
 import TextInput from './widgets/TextInput';
 import Button from './widgets/Button';
-import List from './widgets/List';
+import List, { IOptionEntry } from './widgets/List';
+import IconButton, { IconName } from './widgets/IconButton';
+import { useClickOutside, useFocusOutside } from './utils/utils';
 
-export const OPTIONS = [
-  {
-    value: 'Bass Boost',
-    label: 'Bass Boost',
-    display: 'Bass Boost',
-  },
-  {
-    value: 'Classic',
-    label: 'Classic',
-    display: 'Classic',
-  },
-  {
-    value: 'Dance',
-    label: 'Dance',
-    display: 'Dance',
-  },
-];
+interface IListItemProps {
+  value: string;
+  handleChange: (newValue: string) => void;
+  handleDelete: (value: string) => void;
+  isDisabled: boolean;
+}
+
+// TODO: move this component to a new file
+// TODO: add logic to show icons on hover
+// TODO: figure out why the button clicks aren't working for the icons
+const PresetListItem = ({
+  value,
+  handleChange,
+  handleDelete,
+  isDisabled,
+}: IListItemProps) => {
+  const editModeRef = useRef<HTMLDivElement>(null);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+  // Close edit mode if the user clicks outside of the input
+  useClickOutside<HTMLDivElement>(editModeRef, () => {
+    setIsEditMode(false);
+  });
+
+  // Close edit mode if the user tabs outside of the input
+  useFocusOutside<HTMLDivElement>(editModeRef, () => {
+    setIsEditMode(false);
+  });
+
+  const handleEditClicked = () => {
+    setIsEditMode(true);
+  };
+
+  const handleDeleteClicked = () => {
+    handleDelete(value);
+  };
+
+  const handleInputChange = (newValue: string) => {
+    handleChange(newValue);
+    setIsEditMode(false);
+  };
+
+  return (
+    <div className="row presetListItem">
+      {isEditMode ? (
+        <TextInput
+          value={value}
+          ariaLabel="Edit Preset Name"
+          isDisabled={false}
+          handleChange={handleInputChange}
+        />
+      ) : (
+        <>
+          {value}
+          <div className="row icons">
+            <IconButton
+              icon={IconName.EDIT}
+              handleClick={handleEditClicked}
+              isDisabled={isDisabled}
+            />
+            <IconButton
+              icon={IconName.DELETE}
+              handleClick={handleDeleteClicked}
+              isDisabled={isDisabled}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 const PresetsBar = () => {
   const { globalError } = useAquaContext();
@@ -31,10 +87,65 @@ const PresetsBar = () => {
     string | undefined
   >(undefined);
 
+  const handleCreatePreset = () => {
+    // TODO: add code for creating a preset
+    setSelectedPresetName(presetName);
+  };
+
+  const handleLoadPreset = () => {
+    // TODO: add code for loading a preset
+  };
+
+  const handleChangePresetName = (newValue: string) => {
+    setPresetName(newValue);
+
+    // TODO: If the preset name is an existing one, we should set the selected one to be that so we can load it
+    setSelectedPresetName(undefined);
+  };
+
   const handleChangeSelectedPreset = (newValue: string) => {
     setPresetName(newValue);
     setSelectedPresetName(newValue);
   };
+
+  const [presetNames, setPresetNames] = useState<string[]>([
+    'Bass Boost',
+    'Classic',
+    'Dance',
+  ]);
+
+  const options: IOptionEntry[] = useMemo(() => {
+    const handleEditExistingPresetName = (
+      oldValue: string,
+      newValue: string
+    ) => {
+      console.log('edit');
+      // TODO: improve DS to help check if the newValue is an existing preset
+      setPresetNames(presetNames.map((n) => (n === oldValue ? newValue : n)));
+    };
+
+    const handleDeletePreset = (deletedValue: string) => {
+      console.log('delete');
+      setPresetNames(presetNames.filter((n) => n !== deletedValue));
+    };
+
+    return presetNames.map((n) => {
+      return {
+        value: n,
+        label: n,
+        display: (
+          <PresetListItem
+            value={n}
+            handleChange={(newValue: string) =>
+              handleEditExistingPresetName(n, newValue)
+            }
+            handleDelete={handleDeletePreset}
+            isDisabled={!!globalError}
+          />
+        ),
+      };
+    });
+  }, [globalError, presetNames]);
 
   return (
     <div className="col presetsBar center">
@@ -45,30 +156,30 @@ const PresetsBar = () => {
           value={presetName}
           ariaLabel="Preset Name"
           isDisabled={!!globalError}
-          handleChange={setPresetName}
+          handleChange={handleChangePresetName}
         />
       </div>
       <Button
         ariaLabel="Save settings to preset"
-        className="small"
+        className="small full"
         isDisabled={!!globalError}
-        handleChange={() => console.log('save')}
+        handleChange={handleCreatePreset}
       >
         Save current settings to preset
       </Button>
       <List
         name="preset"
-        options={OPTIONS}
+        options={options}
         className="full"
-        value={selectedPresetName || ''}
+        value={presetName}
         handleChange={handleChangeSelectedPreset}
         isDisabled={!!globalError}
       />
       <Button
         ariaLabel="Load selected preset"
-        className="small"
+        className="small full"
         isDisabled={!!globalError || !selectedPresetName}
-        handleChange={() => console.log('save')}
+        handleChange={handleLoadPreset}
       >
         Load selected preset
       </Button>
