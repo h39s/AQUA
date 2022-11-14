89e6e2f37bea4ff30e705e1f80bf85c3db7a890b
Selina Li <selinali2010@gmail.com>
2022-11-13 17:23:08 -0800

    Add edit mode for preset bar list item

--- src/renderer/widgets/TextInput.tsx
+++ src/renderer/widgets/TextInput.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent } from 'react';
+import {
+  ChangeEvent,
+  ForwardedRef,
+  forwardRef,
+  KeyboardEvent,
+  useEffect,
+  useState,
+} from 'react';
 import '../styles/TextInput.scss';
 
 interface ITextInputProps {
@@ -8,28 +15,43 @@ interface ITextInputProps {
   handleChange: (newValue: string) => void;
 }
 
-const TextInput = ({
-  value,
-  ariaLabel,
-  isDisabled,
-  handleChange,
-}: ITextInputProps) => {
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value: input } = e.target;
-    handleChange(input);
-  };
+const TextInput = forwardRef(
+  (
+    { value, ariaLabel, isDisabled, handleChange }: ITextInputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const [storedValue, setStoredValue] = useState<string>(value);
 
-  return (
-    <input
-      className="textInput"
-      type="text"
-      value={value}
-      aria-label={ariaLabel}
-      onChange={onChange}
-      tabIndex={isDisabled ? -1 : 0}
-      aria-disabled={isDisabled}
-    />
-  );
-};
+    useEffect(() => {
+      setStoredValue(value);
+    }, [value]);
+
+    // Helper for detecting use of the ENTER key
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.code === 'Enter') {
+        handleChange(storedValue);
+      }
+    };
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const { value: input } = e.target;
+      setStoredValue(input);
+    };
+
+    return (
+      <input
+        ref={ref}
+        className="textInput"
+        type="text"
+        value={storedValue}
+        aria-label={ariaLabel}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        tabIndex={isDisabled ? -1 : 0}
+        aria-disabled={isDisabled}
+      />
+    );
+  }
+);
 
 export default TextInput;
