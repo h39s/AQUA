a5afcfc13a8c6451eb91fe5c17e85682949d8c7f
Selina Li <selinali2010@gmail.com>
2023-01-09 16:59:40 -0500

    Refactor Points to create a Point component

--- src/renderer/graph/Points.tsx
+++ src/renderer/graph/Points.tsx
@@ -1,11 +1,75 @@
 import * as d3 from 'd3';
 import { useCallback, useEffect, useMemo, useRef } from 'react';
-import { ChartDataPoint, ChartDataPointWithId } from './ChartController';
+import { ChartDataPointWithId } from './ChartController';
 
 export enum AnimationOptionsEnum {
   FADE_IN = 'fadeIn',
   NONE = 'none',
 }
+
+interface IPointProps {
+  name: string;
+  pointData: ChartDataPointWithId;
+  radius: number;
+  color: string;
+  animation?: AnimationOptionsEnum;
+  transform?: string;
+}
+
+const Point = ({
+  name,
+  pointData,
+  radius,
+  color,
+  animation,
+  transform,
+}: IPointProps) => {
+  const ref = useRef<SVGCircleElement>(null);
+  const { id, x, y } = pointData;
+
+  const animateFadeIn = useCallback(() => {
+    if (ref.current) {
+      d3.select(ref.current)
+        .transition()
+        .duration(750)
+        .ease(d3.easeLinear)
+        .attr('opacity', 1);
+    }
+  }, []);
+
+  const noneAnimation = useCallback(() => {
+    if (ref.current) {
+      d3.select(ref.current).attr('opacity', 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    switch (animation) {
+      case AnimationOptionsEnum.FADE_IN:
+        animateFadeIn();
+        break;
+      case AnimationOptionsEnum.NONE:
+      default:
+        noneAnimation();
+        break;
+    }
+  }, [animateFadeIn, noneAnimation, animation]);
+
+  return (
+    <circle
+      ref={ref}
+      key={id}
+      id={id}
+      name={name}
+      r={radius}
+      fill={color}
+      stroke="#ffffff"
+      transform={transform}
+      cx={x}
+      cy={y}
+    />
+  );
+};
 
 interface IPointsProps {
   name: string;
@@ -26,38 +90,6 @@ const Points = ({
   animation = AnimationOptionsEnum.NONE,
   transform,
 }: IPointsProps) => {
-  const refs = useRef<SVGPathElement[]>([]);
-  useEffect(() => {
-    refs.current = refs.current.slice(0, data.length);
-  }, [data]);
-
-  const animateFadeIn = useCallback(() => {
-    refs.current?.forEach((r) =>
-      d3
-        .select(r)
-        .transition()
-        .duration(750)
-        .ease(d3.easeLinear)
-        .attr('opacity', 1)
-    );
-  }, []);
-
-  const noneAnimation = useCallback(() => {
-    refs.current?.forEach((r) => d3.select(r).attr('opacity', 1));
-  }, []);
-
-  useEffect(() => {
-    switch (animation) {
-      case AnimationOptionsEnum.FADE_IN:
-        animateFadeIn();
-        break;
-      case AnimationOptionsEnum.NONE:
-      default:
-        noneAnimation();
-        break;
-    }
-  }, [animateFadeIn, noneAnimation, animation]);
-
   const scaledPoints = useMemo(
     () =>
       data.map(({ x, y, id }) => {
@@ -70,46 +102,17 @@ const Points = ({
     [data, xScale, yScale]
   );
 
-  // Map points from filter id to coordinate
-  const scaledPointsMap = useMemo(() => {
-    const map: { [id: string]: ChartDataPoint } = {};
-    scaledPoints.forEach((point) => {
-      map[point.id] = { x: point.x, y: point.y };
-    });
-    return map;
-  }, [scaledPoints]);
-
-  // Update point location when changes are made
-  useEffect(() => {
-    if (refs.current) {
-      refs.current
-        // filter out refs for which we don't have coordinates
-        ?.filter(({ id }) => id in scaledPointsMap)
-        .forEach((r) =>
-          d3
-            .select(r)
-            .attr('cx', scaledPointsMap[r.id].x)
-            .attr('cy', scaledPointsMap[r.id].y)
-        );
-    }
-  }, [scaledPointsMap]);
-
   return (
     <>
-      {scaledPoints.map(({ id }, i) => (
-        <circle
-          key={id}
-          id={id}
+      {scaledPoints.map((point) => (
+        <Point
+          key={point.id}
           name={name}
-          ref={(element) => {
-            if (element) {
-              refs.current[i] = element;
-            }
-          }}
-          r={4}
-          fill={color}
-          stroke="#ffffff"
+          pointData={point}
+          radius={4}
+          color={color}
           transform={transform}
+          animation={animation}
         />
       ))}
     </>
